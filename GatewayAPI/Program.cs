acc1eb9e6a95e1031b59133e32c0e95954ec9ff7
abdulhamit <hmd62392@gmail.com>
2024-01-04 23:48:20 +0300

    jaeger added (tracing)

--- GatewayAPI/Program.cs
+++ GatewayAPI/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System.Text;
+using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using OpenTracing;
+using OpenTracing.Contrib.NetCore.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +40,42 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidateLifetime = true
         };
     });
+
+#region Tracing
+
+builder.Services.AddOpenTracing();
+    builder.Services.AddSingleton<ITracer>(sp =>
+    {
+        var serviceName = sp.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+        var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+        var reporter = new RemoteReporter.Builder().WithLoggerFactory(loggerFactory).WithSender(new UdpSender())
+            .Build();
+        var tracer = new Tracer.Builder(serviceName)
+            .WithSampler(new ConstSampler(true))
+            .WithReporter(reporter)
+            .Build();
+        return tracer;
+    });
+
+    builder.Services.Configure<HttpHandlerDiagnosticOptions>(options =>
+        options.OperationNameResolver =
+            request => $"{request.Method.Method}: {request?.RequestUri?.AbsoluteUri}");
+
+builder.Services.AddOpenTracing();
+
+builder.Services.Configure<AspNetCoreDiagnosticOptions>(options =>
+{
+    options.Hosting.IgnorePatterns.Add(context => context.Request.Path.Value.StartsWith("/status"));
+    options.Hosting.IgnorePatterns.Add(context => context.Request.Path.Value.StartsWith("/metrics"));
+    options.Hosting.IgnorePatterns.Add(context => context.Request.Path.Value.StartsWith("/swagger"));
+});
+
+builder.Services.Configure<HttpHandlerDiagnosticOptions>(options =>
+        options.OperationNameResolver =
+            request => $"{request.Method.Method}: {request?.RequestUri?.AbsoluteUri}");
+
+#endregion
+
 
 builder.Services.AddAuthorization(options =>
 {
