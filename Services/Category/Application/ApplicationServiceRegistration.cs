69db56725f94e056909bfb3b6b3e59bb6c746042
abdulhamit <hmd62392@gmail.com>
2024-10-08 00:06:44 +0300

    Add Application.csproj and ApplicationServiceRegistration.cs

---
+++ Services/Category/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Nucleo.DDD.Application.Pipelines.Validation;
+namespace Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            configuration.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+        });
+        
+        return services;
+    }
+}
