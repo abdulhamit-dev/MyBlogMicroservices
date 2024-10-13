2d98cb7e7f95e31d79e36297feffce8eb5301ec2
abdulhamit <hmd62392@gmail.com>
2024-10-13 14:42:50 +0300

    category for mapping

--- Services/Category/Application/Features/Profiles/MappingProfiles.cs
+++ Services/Category/Application/Features/Profiles/MappingProfiles.cs
@@ -1,4 +1,7 @@
 using Application.Features.Categories.Commands.Create;
+using Application.Features.Categories.Commands.Delete;
+using Application.Features.Categories.Commands.Update;
+using Application.Features.Categories.Queries.GetById;
 using Application.Features.Categories.Queries.GetList;
 using AutoMapper;
 using Domain.Entities;
@@ -11,6 +14,11 @@ public class MappingProfiles:Profile
     {
         CreateMap<Category, CreateCategoryCommand>().ReverseMap();
         CreateMap<Category, CreateCategoryResponse>().ReverseMap();
-        CreateMap<Category, GetListCategoryDto>().ReverseMap();
+        CreateMap<Category, UpdateCategoryCommand>().ReverseMap();
+        CreateMap<Category, UpdateCategoryResponse>().ReverseMap();
+        CreateMap<Category, DeleteCategoryResponse>().ReverseMap();
+        CreateMap<Category, GetByIdCategoryResponse>().ReverseMap();
+        CreateMap<Category, GetListCategoryResponse>().ReverseMap();
+
     }
 }
